d66f92d138ce5d41a7fdc0e124adc0f87743f37f
victorbecerra <vbecerraar@gmail.com>
2025-10-14 15:34:40 +0200

    updated manifests

---
+++ prometheus-grafana-loki/prometheus-operator.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+
+git clone https://github.com/prometheus-operator/kube-prometheus.git
+cd kube-prometheus
+kubectl create -f manifests/setup
+until kubectl get servicemonitors --all-namespaces ; do date; sleep 1; echo ""; done
+kubectl create -f manifests/
